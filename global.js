4043809ae2f1f5980cf2cf6081e047c8184ad4c7
Martinius <mar109us@gmail.com>
2026-03-27 14:21:28 +0100

    updating browser inner size to global variables

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-window.global = {
+window.g = {
 
     min_width: 0,
     max_width: window.innerWidth,
@@ -6,6 +6,7 @@ window.global = {
     min_height: 0,
     max_height: window.innerHeight,
 
+    fps_2: 1000 / 2,
     fps_30: 1000 / 30,
     fps_60: 1000 / 60,
     fps_90: 1000 / 90,
@@ -13,20 +14,20 @@ window.global = {
 
 };
 
-setInterval(global_test, global.fps_60)
+setInterval(update_window_size, g.fps_2)
 
+setInterval(check_value, g.fps_2)
 
-function global_test() {
+function update_window_size() {
+    g.max_width = window.innerWidth
+    g.max_height = window.innerHeight
+}
 
-console.log(
-    global.min_width,
-    global.max_width,
-    global.min_height,
-    global.max_height
-)
-
-
-
-
-
+function check_value() {
+    console.log(
+        g.min_width,
+        g.max_width,
+        g.min_height,
+        g.max_height
+    )
 }
