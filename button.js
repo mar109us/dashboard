6cb9cfb7acfa0781ee645fb2f3ba5075c9142ca2
Martinius <mar109us@gmail.com>
2026-03-27 14:54:22 +0100

    adding button logic.
    adding settings.json to exlude variables from other projects in same workspace

---
+++ button.js
@@ -0,0 +1,34 @@
+class Button {
+    constructor(x, y, w, h) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+
+        this.canvas = document.createElement('canvas')
+        this.button = this.canvas.getContext('2d')
+
+        this.canvas.style.position = "absolute"
+
+        this.canvas.style.left = `${this.x}px`
+        this.canvas.style.top = `${this.y}px`
+        this.canvas.width = this.w
+        this.canvas.height = this.h
+
+        this.canvas.style.zIndex = "10"
+    }
+
+    button_loop() {
+        let button = this.button
+        let x = this.x
+        let 
+    }
+
+
+
+
+
+
+
+
+}
